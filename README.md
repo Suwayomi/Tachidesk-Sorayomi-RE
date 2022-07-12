37eb0492d349c5e4dac0f00ca498e74b1445f9f3
DattatreyaReddy Panta <58727124+DattatreyaReddy@users.noreply.github.com>
2022-07-12 08:11:34 +0530

    Init Project (#1)
    * init remaster
    
    * Init repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tachidesk Sorayomi Re
+
+`Tachidesk Sorayomi Re` is a rewrite of Tachidesk Sorayomi.
---
+++ android/app/src/main/kotlin/com/example/tachidesk_sorayomi_re/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tachidesk_sorayomi_re
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
